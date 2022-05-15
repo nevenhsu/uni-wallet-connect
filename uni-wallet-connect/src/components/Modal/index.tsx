b7df2e8ef7b5274d53cdfddb374c62f5c5448d46
nevenhsu <space.man0318@gmail.com>
2022-05-16 00:32:41 +0800

    fix errors in peerDependencies

--- uni-wallet-connect/src/components/Modal/index.tsx
+++ uni-wallet-connect/src/components/Modal/index.tsx
@@ -67,6 +67,8 @@ const StyledDialogContent = styled(({ minHeight, maxHeight, mobile, isOpen, ...r
         css`
           width: 100vw;
           border-radius: 20px;
+          border-bottom-left-radius: 0;
+          border-bottom-right-radius: 0;
         `
       }
     `}
